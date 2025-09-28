724c86905a7d6b31c7de2632f29cef526018651b
Hayato Wakasugi <Wak8810@users.noreply.github.com>
2025-09-28 12:41:48 +0900

    logファイルの追跡を削除

--- fuel/app/logs/2025/08/11.php
+++
@@ -1,4 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-WARNING - 2025-08-11 17:42:57 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2025-08-11 17:43:01 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.

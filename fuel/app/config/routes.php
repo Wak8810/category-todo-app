4f62519cf406661c30cfee48500335454573d218
Hayato Wakasugi <Wak8810@users.noreply.github.com>
2025-10-10 10:15:46 +0900

    feat: ルーティング設定を追加

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -2,6 +2,10 @@
 return array(
 	'_root_'  => 'welcome/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
+
+	'register' => array(array('GET', new Route('register/index')), array('POST', new Route('register/register'))),
+
+	'login' => array(array('GET', new Route('login/index')), array('POST', new Route('login/login'))),
 	
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );

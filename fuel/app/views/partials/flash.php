6d1101134e11dd7c92e44593f3472a30780bc815
Hayato Wakasugi <Wak8810@users.noreply.github.com>
2025-10-20 10:02:03 +0900

    feat: XSS対策を追加
    flashメッセージで対策していなかったので、e()で対策

--- fuel/app/views/partials/flash.php
+++ fuel/app/views/partials/flash.php
@@ -3,16 +3,16 @@ use Fuel\Core\Session;
 ?>
 
 <?php if ($success = Session::get_flash('success')) : ?>
-  <div class="p-15 rounded-4 mb-20 text-green-dark bg-green-pale border border-green-pale"><?php echo $success; ?></div>
+  <div class="p-15 rounded-4 mb-20 text-green-dark bg-green-pale border border-green-pale"><?php echo e($success); ?></div>
 <?php endif; ?>
 <?php if ($error = Session::get_flash('error')) : ?>
-  <div class="p-15 rounded-4 mb-20 text-red-dark bg-red-light border border-red-light"><?php echo $error; ?></div>
+  <div class="p-15 rounded-4 mb-20 text-red-dark bg-red-light border border-red-light"><?php echo e($error); ?></div>
 <?php endif; ?>
 <?php if ($errors = Session::get_flash('errors')) : ?>
   <div class="p-15 mb-20 rounded-4 text-red-dark bg-red-light border border-red-light">
     <ul>
       <?php foreach ($errors as $error_message) : ?>
-        <li><?php echo $error_message; ?></li>
+        <li><?php echo e($error_message); ?></li>
       <?php endforeach; ?>
     </ul>
   </div>

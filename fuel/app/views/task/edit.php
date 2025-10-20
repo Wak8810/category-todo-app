059818a2f7e7d8a5393d748cc20d2b9a46b67a5d
Hayato Wakasugi <Wak8810@users.noreply.github.com>
2025-10-20 14:05:47 +0900

    feat: タスク編集画面にもバリデーションを表示

--- fuel/app/views/task/edit.php
+++ fuel/app/views/task/edit.php
@@ -16,23 +16,25 @@ use Fuel\Core\View;
   <?php echo View::forge('partials/header'); ?>
 
   <div class="p-x-20">
-    <div class="max-w-600 m-x-auto mt-30 p-50 bg-white rounded-8 shadow-md">
+    <div id="edit-task-view" class="max-w-600 m-x-auto mt-30 p-50 bg-white rounded-8 shadow-md"
+         data-task='<?php echo json_encode($task, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT); ?>'>
       <h2 class="mb-40 font-size-xl font-weight-bold">タスク編集</h2>
 
       <?php echo View::forge('partials/flash'); ?>
 
       <?php $inputs = isset($form_inputs) ? $form_inputs : []; ?>
 
-      <form id="update-task-form" action="<?php echo Uri::create('tasks/update/' . $task['id']); ?>" method="POST">
+      <form id="update-task-form" method="POST" data-bind="attr: { action: updateUrl }">
         <?php echo Form::csrf(); ?>
         <div class="mb-25">
           <label for="title" class="d-block mb-8 font-weight-600">タスク名</label>
-          <textarea name="title" id="title" class="w-100p p-y-10 p-x-20 rounded-6 border border-gray-medium resize-vertical" rows="3" required><?php echo e(Arr::get($inputs, 'title', $task['title'])); ?></textarea>
+          <textarea name="title" id="title" class="w-100p p-y-10 p-x-20 rounded-6 border border-gray-medium resize-vertical" rows="3" required data-bind="value: title, valueUpdate: 'afterkeydown'"><?php echo e(Arr::get($inputs, 'title', $task['title'])); ?></textarea>
+          <div class="font-size-xs mt-5 text-red min-h-1-2em" data-bind="visible: titleError, text: titleError"></div>
         </div>
 
         <div class="mb-40">
           <label for="category_id" class="d-block mb-8 font-weight-600">カテゴリー</label>
-          <select name="category_id" id="category_id" class="w-100p p-y-10 p-x-20 rounded-6 border border-gray-medium">
+          <select name="category_id" id="category_id" class="w-100p p-y-10 p-x-20 rounded-6 border border-gray-medium" data-bind="value: category_id">
             <?php foreach ($categories as $category):
               $selected_category = Arr::get($inputs, 'category_id', $task['category_id']);
               $is_selected = ($selected_category == $category['id']);
@@ -50,11 +52,13 @@ use Fuel\Core\View;
         </form>
         <div>
           <a href="<?php echo Uri::create('tasks'); ?>" class="p-y-10 p-x-20 rounded-6 text-decoration-none bg-gray-dark text-white mr-12">キャンセル</a>
-          <button type="submit" form="update-task-form" class="p-y-10 p-x-20 rounded-6 border-none bg-blue text-white font-weight-bold cursor-pointer">更新</button>
+          <button type="submit" form="update-task-form" class="p-y-10 p-x-20 rounded-6 border-none bg-blue text-white font-weight-bold cursor-pointer" data-bind="enable: isFormValid">更新</button>
         </div>
       </div>
     </div>
   </div>
 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.1/knockout-latest.js"></script>
+  <?php echo Asset::js('edit_task.js'); ?>
 </body>
 </html>
